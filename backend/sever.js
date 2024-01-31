c789236bfcc6d5740819e2c0e9f8be5bd0764fb0
achyut vardhan <achyutvardhan234@gmail.com>
2024-01-31 22:43:49 +0530

    update

--- backend/sever.js
+++ backend/sever.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express();
 const cors  = require('cors')
-
+const mongoose = require('mongoose');
 app.use(express.json())
 app.use(cors)
+
+mongoose.connect()
+
+app.get('*', (req, res)=>{
+   res.status(404).send("webpage doesn't exist");
+})
+
+app.listen(8000, ()=>{
+    console.log("listening to port 8000..")
+})
