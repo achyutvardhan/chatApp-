d92c6b9173753264a2a3438def0929b0d7e6f830
achyut vardhan <achyutvardhan234@gmail.com>
2024-02-17 22:29:42 +0530

    .

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import prf from '../assets/profile.svg'
-import '../css/ChatHeader.css'
+import React from "react";
+import prf from "../assets/profile.svg";
+import "../css/ChatHeader.css";
+// import { useContext } from "react";
+// import { AuthContext } from "../AuthContext";
 export default function ChatHeader() {
+  // const { user } = useContext(AuthContext);
+  // console.log(user);
   return (
     <>
-    <div className="exthead">
+      <div className="exthead">
         <div className="inhead">
-            <img src={prf} alt="" width={50} height={50} />
-            <p>Name</p>
+          <img src={prf} alt="" width={50} height={50} />
+          <p>Name</p>
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
