542f9d8da8ead712e5251b6ae98b70ac5d98e689
achyut vardhan <achyutvardhan234@gmail.com>
2024-02-19 09:37:57 +0530

    chats added

--- frontend/src/components/Rec.jsx
+++ frontend/src/components/Rec.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import '../css/Rec.css'
-export default function Rec() {
+import React from "react";
+import "../css/Rec.css";
+export default function Rec({ data }) {
+  // console.log(data);
+  let date = new Date(data.timestamp);
+
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
   return (
     <>
-    <div className="recext">
+      <div className="recext">
         <div className="recint">
-            <div className="namerec"><strong>Achyut Vardhan</strong></div>
-            <div className="recmsg">hello ! my name is achyut</div>
-            <div className="recdtl">
-                <div className="rectme">12:00</div>
-                <div className="msgphase">//</div>
-            </div>
+          {/* <div className="namerec">
+            <strong>Achyut Vardhan</strong>
+          </div> */}
+          <div className="recmsg"> {data.message}</div>
+          <div className="recdtl">
+            <div className="rectme">{hours}:{minutes}</div>
+            <div className="msgphase">//</div>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
