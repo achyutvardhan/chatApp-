10fd54fe111120416c358d346eeb1dc641723829
achyut vardhan <achyutvardhan234@gmail.com>
2024-11-20 15:46:00 +0530

    removed middleware

--- backend/routers/Auth/protectedR.js
+++ backend/routers/Auth/protectedR.js
@@ -6,7 +6,7 @@ const {readMessage} = require("../../controller/readMessage")
 const {editProfile} = require("../../controller/editProfile")
 const {getAllUser} = require("../../controller/getAllUser");
 const { checkSmsClassifier } = require('../../middleware/CheckSmsClassifier');
-router.post('/sendChat/:id',checkTokenExpiration, checkSmsClassifier, sendMessage);
+router.post('/sendChat/:id',checkTokenExpiration, sendMessage);
 router.post('/getChat/:id',checkTokenExpiration ,readMessage);
 router.post('/updateProfile/:id',checkTokenExpiration ,editProfile);
 router.get('/getallUser/:id',checkTokenExpiration, getAllUser);
