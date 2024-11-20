20d403cfe49b6e67af8858b33d1fa22ce034e2a8
achyut vardhan <achyutvardhan234@gmail.com>
2024-11-20 10:46:17 +0530

    axios addded

--- backend/middleware/CheckSmsClassifier.js
+++ backend/middleware/CheckSmsClassifier.js
@@ -1,22 +1,11 @@
-const fetch = require('node-fetch')
+const axios = require('axios');
+
 const checkSmsClassifier = async (req, res, next) => {
     const data = req.body.data;
     try {
-        const response = await fetch("http://127.0.0.1:5120/predict", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ data }),
-        });
+        const response = await axios.post("http://127.0.0.1:5120/predict", { data });
 
-        // if (!response.ok) {
-        //     throw new Error(`HTTP error! status: ${response.status}`);
-        // }
-
-        const result = await response.json();
-        console.log(result);
-        // req.classifierResult = result;
+        console.log(response.data);
         next();
     } catch (error) {
         console.error("Error in checkSmsClassifier:", error);
